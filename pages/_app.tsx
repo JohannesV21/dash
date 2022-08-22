ea4b7d3445085acb31670e0d081f91d0b2b3b35c
Braulio Chavez <bjcdevelop@gmail.com>
2022-08-22 10:28:17 -0400

    Favicon Add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,6 +18,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Dashboard Estelio</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="Icon" href="/icono.ico" />
       </Head>
       <ChakraProvider theme={theme}>
           <Component {...pageProps} />
